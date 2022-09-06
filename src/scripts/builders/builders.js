a31baa6c4c9757b70af789abff617ebe47c7cc9b
Runite Drill <camielplaysgames@gmail.com>
2022-09-05 23:23:16 -0700

    hoi4 build script

--- src/scripts/builders/builders.js
+++ src/scripts/builders/builders.js
@@ -1,11 +1,13 @@
 import { eu4_builder } from "./eu4_builder"
 import { ck3_builder } from "./ck3_builder"
 import { ck2_builder } from "./ck2_builder"
+import { hoi4_builder } from "./hoi4_builder"
 import { ir_builder } from "./ir_builder"
 
 export const builders = {
   eu4_builder,
   ck3_builder,
   ck2_builder,
+  hoi4_builder,
   ir_builder,
 }
---
+++ src/scripts/builders/hoi4_builder.js
@@ -0,0 +1,240 @@
+export function hoi4_builder(mod, files, gameFolder) {
+  /*
+  # MUSIC MOD CREATION TOOL (MMCT) for Hearts of Iron 4
+
+  # This is a script that creates the required .asset file and a basic .txt file for your Hearts of Iron 4 Music Mod based on the .ogg files passed into it.
+  
+  # The script will skip over any file called "maintheme" as this only replaces the loading music and is not shown in-game by the audio engine.
+  # You are strongly encouraged to hop into the generated files and check that everything has been created properly. In particular, you may wish to add to the conditions in the .txt file that affect the chances of the music playing.
+  
+  # This script is open-source and was created by community composer Runite Drill / Utopia for ease in publishing music mods.
+  # Feel free to join their music modding community and ask any questions: https://discord.gg/SdQhfBM
+  
+  # For more info/guide on music modding, please visit the EU4 wiki: https://hoi4.paradoxwikis.com/Music_modding
+  */
+
+  console.log('HOI4!')
+  const gfxFolder = gameFolder.folder("gfx")
+  const interfaceFolder = gameFolder.folder("interface")
+  const localisationFolder = gameFolder.folder("localisation")
+  const musicFolder = gameFolder.folder("music")
+
+  const stationName = `${mod}_MMCT`
+
+  //Create gfx files
+  gfxFolder.file(`radio_station_cover_${stationName}.dds`, require('../../data/hoi4_radio_station.dds'));
+  gfxFolder.file(`radio_station_cover_template.png`, require('../../data/radio_station_cover_template.png'));
+
+  //Create interface files
+  interfaceFolder.file(`${stationName}.gfx`, gfxDef(stationName));
+  interfaceFolder.file(`${stationName}.gui`, guiDef(stationName));
+
+  //Create localisation files
+  const loc = locDef(files, mod, stationName)
+  localisationFolder.file(`music_station_${stationName}_l_english.yml`, loc.join('\n'));
+
+  //Create music files
+  const modMusicFolder = musicFolder.folder(stationName)
+
+  //.asset file
+  const asset = [assetHeader]
+  files.forEach((f) => {
+    if (f.name.split(".")[0] !== "maintheme") {
+      asset.push(assetDef(f))
+    }
+    modMusicFolder.file(f.name, f)
+  })
+  modMusicFolder.file(`${mod}_MMCT.asset`, asset.join('\n'));
+
+  //.txt file
+  const text = [textHeader, `music_station = "${stationName}"`]
+  files.forEach((f) => {
+    if (f.name.split(".")[0] !== "maintheme") {
+      text.push(textDef(f))
+    }
+  })
+  modMusicFolder.file(`${mod}_MMCT.txt`, text.join('\n'));
+
+  return gameFolder
+}
+
+function gfxDef(stationName) {
+  return `spriteTypes = {
+    spriteType = {
+      name = "GFX_radio_station_cover_${stationName}"
+      textureFile = "gfx/radio_station_cover_${stationName}.dds"
+      noOfFrames = 2
+    }
+}`
+}
+
+function guiDef(stationName) {
+  return `guiTypes = {
+    containerWindowType = {
+        name = "${stationName}_faceplate"
+        position = { x=0 y=0 }
+        size = { width = 590 height = 46 }
+  
+        iconType ={
+            name ="musicplayer_header_bg"
+            spriteType = "GFX_musicplayer_header_bg"
+            position = { x= 0 y = 0 }
+            alwaystransparent = yes
+        }
+        
+        instantTextboxType = {
+            name = "track_name"
+            position = { x = 72 y = 20 }
+            font = "hoi_18b"
+            text = "Roger Pontare - Nar vindarna viskar mitt namn"
+            maxWidth = 450
+            maxHeight = 25
+            format = center
+        }
+    
+        instantTextboxType = {
+            name = "track_elapsed"
+            position = { x = 124 y = 30 }
+            font = "hoi_18b"
+            text = "00:00"
+            maxWidth = 50
+            maxHeight = 25
+            format = center
+        }
+    
+        instantTextboxType = {
+            name = "track_duration"
+            position = { x = 420 y = 30 }
+            font = "hoi_18b"
+            text = "02:58"
+            maxWidth = 50
+            maxHeight = 25
+            format = center
+        }
+  
+        buttonType = {
+            name = "prev_button"
+            position = { x = 220 y = 20 }
+            quadTextureSprite ="GFX_musicplayer_previous_button"
+            buttonFont = "Main_14_black"
+            Orientation = "LOWER_LEFT"
+            clicksound = click_close
+            pdx_tooltip = "MUSICPLAYER_PREV"
+        }
+    
+        buttonType = {
+            name = "play_button"
+            position = { x = 263 y = 20 }
+            quadTextureSprite ="GFX_musicplayer_play_pause_button"
+            buttonFont = "Main_14_black"
+            Orientation = "LOWER_LEFT"
+            clicksound = click_close
+        }
+    
+        buttonType = {
+            name = "next_button"
+            position = { x = 336 y = 20 }
+            quadTextureSprite ="GFX_musicplayer_next_button"
+            buttonFont = "Main_14_black"
+            Orientation = "LOWER_LEFT"
+            clicksound = click_close
+            pdx_tooltip = "MUSICPLAYER_NEXT"
+        }
+    
+        extendedScrollbarType = {
+            name = "volume_slider"
+            position = { x = 100 y = 45}
+            size = { width = 75 height = 18 }
+            tileSize = { width = 12 height = 12}
+            maxValue =100
+            minValue =0
+            stepSize =1
+            startValue = 50
+            horizontal = yes
+            orientation = lower_left
+            origo = lower_left
+            setTrackFrameOnChange = yes
+    
+            slider = {
+                name = "Slider"	
+                quadTextureSprite = "GFX_scroll_drager"
+                position = { x=0 y = 1 }
+                pdx_tooltip = "MUSICPLAYER_ADJUST_VOL"
+            }
+    
+            track = {
+                name = "Track"
+                quadTextureSprite = "GFX_volume_track"
+                position = { x=0 y = 3 }
+                alwaystransparent = yes
+                pdx_tooltip = "MUSICPLAYER_ADJUST_VOL"
+            }
+        }
+  
+        buttonType = {
+            name = "shuffle_button"
+            position = { x = 425 y = 20 }
+            quadTextureSprite ="GFX_toggle_shuffle_buttons"
+            buttonFont = "Main_14_black"
+            Orientation = "LOWER_LEFT"
+            clicksound = click_close
+        }
+    }
+  
+    containerWindowType={
+        name = "${stationName}_stations_entry"
+        size = { width = 162 height = 130 }
+        
+        checkBoxType = {
+            name = "select_station_button"
+            position = { x = 0 y = 0 }
+            quadTextureSprite = "GFX_radio_station_cover_${stationName}"
+            clicksound = decisions_ui_button
+        }
+    }
+}`  
+}
+
+function locDef(files, mod, stationName) {
+  const text = [`l_english:
+ ${stationName}_TITLE:0 "${mod} Radio"`]
+ files.forEach((f) => {
+  text.push(` ${f.name.split(".")[0]}_MMCT:0 "${f.name.split(".")[0]}"`)
+ })
+
+ return text
+}
+
+const assetHeader = `#This file was automatically generated by the Music Mod Creation Tool (MMCT), created by Runite Drill / Utopia.
+#Consider joining the music modding discord server, Utopia, if you have any questions: https://discord.gg/SdQhfBM.
+#Use the Music Mod Creation Tool to generate your own music mods for all Paradox Interactive games: <link tba>
+  
+#Please confirm the output is as expected before testing in-game.
+#And remember to change the .dds of your radio station thumbnail in the gfx folder!
+`
+
+function assetDef(file) {
+  return `music = {
+    name = "${file.name.split(".")[0]}_MMCT"
+    file = "${file.name}"
+    volume = 0.65
+}\n`
+}
+
+const textHeader = assetHeader + `
+#In this file you can specify the triggers, factors and conditions in which the music will play in-game.
+#Please also remember to change your radio station image by creating a new .dds file. The template .png has been provided in the gfx folder for your convenience.
+#You may also with to fix up some of the localisation of the songs / radio station. You can do this by editing the .yml in the localisation folder.
+
+#Consult the music modding page on the HOI4 wiki for more info: https://hoi4.paradoxwikis.com/Music_modding.
+`
+
+function textDef(file) {
+  return `music = {
+    song = "${file.name.split(".")[0]}_MMCT"
+    chance = {
+      modifier = { factor = 1 }
+      #modifier = { factor = 0 has_war = yes } #example modifier: music will not play when at war
+    }
+}\n`
+}
