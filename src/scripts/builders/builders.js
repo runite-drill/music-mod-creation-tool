e0683f1fb8acdbbc969d86b95300087748d74c4d
Runite Drill <camielplaysgames@gmail.com>
2022-09-05 22:02:32 -0700

    Imperator build script

--- src/scripts/builders/builders.js
+++ src/scripts/builders/builders.js
@@ -1,7 +1,9 @@
 import { eu4_builder } from "./eu4_builder"
 import { ck3_builder } from "./ck3_builder"
+import { ir_builder } from "./ir_builder"
 
 export const builders = {
   eu4_builder,
   ck3_builder,
+  ir_builder,
 }
