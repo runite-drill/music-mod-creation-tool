ea4a0b4a892f7743863e593091129b03ed659e19
Runite Drill <camielplaysgames@gmail.com>
2022-09-05 22:16:07 -0700

    ck2 build script

--- src/scripts/builders/builders.js
+++ src/scripts/builders/builders.js
@@ -1,9 +1,11 @@
 import { eu4_builder } from "./eu4_builder"
 import { ck3_builder } from "./ck3_builder"
+import { ck2_builder } from "./ck2_builder"
 import { ir_builder } from "./ir_builder"
 
 export const builders = {
   eu4_builder,
   ck3_builder,
+  ck2_builder,
   ir_builder,
 }
